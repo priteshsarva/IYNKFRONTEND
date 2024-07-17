4e83640abf4ac1deaea27e3774a79f8e428332fc
priteshsarva <priteshsarva9825@gmail.com>
2024-07-17 16:05:12 +0530

    all good

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
   return (
     <>
 
-      {/* <BrowserRouter>
+      <BrowserRouter>
         <NavBar />
         <Routes>
 
@@ -20,9 +20,8 @@ function App() {
           <Route path="/reddit" element={<Reddit />} key="3" />
 
         </Routes>
-      </ BrowserRouter> */}
-HEEELLLOOO 
-22
+      </ BrowserRouter>
+
 
     </>
   );
