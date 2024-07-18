11e356b322c78239285537d5f762e26cddf4025d
priteshsarva <priteshsarva9825@gmail.com>
2024-07-18 12:01:24 +0530

    updated changes

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import Category from './components/Category';
 import Home from './components/Home';
 import NavBar from "./components/NavBar"
-import { HashRouter, BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Reddit from './components/Reddit';
 
 
@@ -11,19 +11,19 @@ function App() {
   return (
     <>
 
-      <HashRouter>
-        {/* <BrowserRouter basename='/iynk'> */}
+
+      <BrowserRouter basename='/'>
 
         <NavBar />
         <Routes>
 
-          <Route path="/" element={<Home />}  />
+          <Route path="/" element={<Home />} />
           <Route path="/category" element={<Category />} />
           <Route path="/reddit" element={<Reddit />} />
 
         </Routes>
-        {/* </ BrowserRouter> */}
-      </ HashRouter>
+      </ BrowserRouter>
+
 
 
 
